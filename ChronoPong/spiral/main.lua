7da73f07406bf57e087011b7a9892095cfde3f15
slofmb <loizoustephanos@gmail.com>
2014-12-29 20:43:05 +0200

    some cleanup for spiral
    Signed-off-by: slofmb <loizoustephanos@gmail.com>

--- ChronoPong/spiral/main.lua
+++ ChronoPong/spiral/main.lua
@@ -2,28 +2,21 @@ local angle=-90
 local seconds={}
 local current=1
 local mins=0
-local rotTime = 3000
+local rotTime = 6000
 while angle<270+720 do
-	
+	local ind = ((angle+90)/6)+1
 	local x1,y1,second
 	x1=320+250*math.cos(angle*2*math.pi/360)
-	--if(angle<180) then
-	
 	y1=480+250*math.sin(angle*2*math.pi/360)
-	
-	--else
-		--y1=200+(300+200*math.sin(angle))
-	--	y1=0
-		
-	--end
-		
+
 	--seconds[((angle+90)/6)+1]=display.newText((angle+90)/6,x1,y1,native.systemFont, 20)
-	seconds[((angle+90)/6)+1]=display.newRect(x1,y1,40, 5)
-	seconds[((angle+90)/6)+1].rotation = angle
+	seconds[ind]=display.newRect(x1,y1,40, 5)
+	seconds[ind].rotation = angle
+	seconds[ind]:setFillColor(0,0,1)
+	seconds[ind].alpha = 0.5
 	angle=angle+6
 end
 
---transitionpar = {time = rotTime, x = 640/2, y = 960/2,alpha = 0, iterations = 0,}
 for i=1 ,180 do
 	transitionpar = {time = rotTime, x = 640/2, y = 960/2,alpha = 0, iterations = 0,delay = i * rotTime/180}
 	transition.to(seconds[i],transitionpar)
